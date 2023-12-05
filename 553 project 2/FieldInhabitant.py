f73e65889a9a7bf4489e8703383d55c04b5f76d8
plkjiet <70952104+plkjiet@users.noreply.github.com>
2023-12-05 16:38:57 -0500

    Add files via upload
    partially implemented

---
+++ 553 project 2/FieldInhabitant.py
@@ -0,0 +1,6 @@
+class FieldInhabitant:
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def get_symbol(self):
+        return self.symbol
---
+++ 553 project 2/main.py
@@ -0,0 +1,8 @@
+from GameEngine import GameEngine
+
+def main():
+    game = GameEngine()
+    game.start()
+
+if __name__ == "__main__":
+    main()
