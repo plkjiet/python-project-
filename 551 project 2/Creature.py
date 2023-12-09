fee024406f6d13d7e991b81e333f97e827467fd7
plkjiet <70952104+plkjiet@users.noreply.github.com>
2023-12-09 16:46:35 -0500

    Update Creature.py

--- 551 project 2/Creature.py
+++ 551 project 2/Creature.py
@@ -2,11 +2,5 @@
 from FieldInhabitant import FieldInhabitant
 
 class Creature(FieldInhabitant):
-    def __init__(self, x, y, symbol):
-        super().__init__(symbol)
-        self.x = x
-        self.y = y
-
-    def move(self, new_x, new_y):
-        self.x = new_x
-        self.y = new_y
+    def __init__(self, symbol, x, y):
+        super().__init__(symbol, x, y)  # Initialize the base class
