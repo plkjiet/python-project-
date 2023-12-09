643a47ef7f285d5ff41269c3d3406e29a5453873
plkjiet <70952104+plkjiet@users.noreply.github.com>
2023-12-09 16:47:40 -0500

    Update GameEngine.py

--- 551 project 2/GameEngine.py
+++ 551 project 2/GameEngine.py
@@ -1,83 +1,72 @@
 #Zhongyi Wang
+import csv
+from Veggie import Veggie
 from Captain import Captain
 from Rabbit import Rabbit
-from Veggie import Veggie
 import random
+
 class GameEngine:
     def __init__(self):
-        self.field_size = 10
-        self.field = [[None for _ in range(self.field_size)] for _ in range(self.field_size)]
+        self.garden = []  # Represents the game field
+        self.captain = None  # Captain of the game
+        self.rabbits = []  # List of rabbits
+        self.veggies = []  # List of vegetables
+
+    def load_game_config(self, filepath):
+        # Load game configuration from a CSV file
+        with open(filepath, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == 'Field Size':
+                    self.garden = [['.' for _ in range(int(row[2]))] for _ in range(int(row[1]))]
+                else:
+                    self.veggies.append(Veggie(row[0], int(row[2]), int(row[3]), int(row[4])))
+
+        # Set initial position for Captain and Rabbits
         self.captain = Captain(0, 0)
-        self.rabbits = [Rabbit(9, 9)]
-        self.veggies = [Veggie("Carrot", "C", 5), Veggie("Lettuce", "L", 10)]
-        self.initGame()
+        for _ in range(2):  # Assuming two rabbits for example
+            self.rabbits.append(Rabbit(random.randint(0, len(self.garden) - 1), random.randint(0, len(self.garden[0]) - 1)))
 
-    def initGame(self):
-        # Initialize veggies at random positions
+    def run_game(self):
+        print("Welcome to Captain Veggie game!")
+        self.load_game_config("VeggieFile.csv")
+        while not self.check_game_over():
+            self.render_game()
+            self.process_user_input()
+            self.update_game_state()
+        print(f"Game over! Your score is: {self.captain.score}")
+
+    def process_user_input(self):
+        # Process player's move command
+        move = input("Enter move command (e.g. 'up', 'down', 'left', 'right'): ")
+        # Boundary check for movement
+        if move == "up" and self.captain.y > 0:
+            self.captain.move(0, -1)
+        elif move == "down" and self.captain.y < len(self.garden) - 1:
+            self.captain.move(0, 1)
+        elif move == "left" and self.captain.x > 0:
+            self.captain.move(-1, 0)
+        elif move == "right" and self.captain.x < len(self.garden[0]) - 1:
+            self.captain.move(1, 0)
+
+    def update_game_state(self):
+        # Update game state for each turn
+        for rabbit in self.rabbits:
+            rabbit.move_randomly(len(self.garden), len(self.garden[0]), self.veggies)
+        self.captain.harvest(self)
+
+    def render_game(self):
+        # Render the game field
+        field = [['.' for _ in range(len(self.garden[0]))] for _ in range(len(self.garden))]
         for veggie in self.veggies:
-            x, y = random.randint(0, self.field_size - 1), random.randint(0, self.field_size - 1)
-            while self.field[x][y] is not None:
-                x, y = random.randint(0, self.field_size - 1), random.randint(0, self.field_size - 1)
-            self.field[x][y] = veggie
-
-        # Place Captain and Rabbit
-        self.field[self.captain.x][self.captain.y] = self.captain
+            field[veggie.x][veggie.y] = veggie.symbol
         for rabbit in self.rabbits:
-            self.field[rabbit.x][rabbit.y] = rabbit
+            field[rabbit.x][rabbit.y] = 'R'
+        field[self.captain.x][self.captain.y] = 'V'
+        for row in field:
+            print(' '.join(row))
+        print(f"Score: {self.captain.score}")
 
-    def printField(self):
-        for row in self.field:
-            print(' '.join([inhabitant.get_symbol() if inhabitant else '.' for inhabitant in row]))
-
-    def moveCaptain(self, direction):
-        # Remove captain from current position
-        self.field[self.captain.x][self.captain.y] = None
-
-        # Update captain's position based on direction
-        if direction == "up" and self.captain.x > 0:
-            self.captain.x -= 1
-        elif direction == "down" and self.captain.x < self.field_size - 1:
-            self.captain.x += 1
-        elif direction == "left" and self.captain.y > 0:
-            self.captain.y -= 1
-        elif direction == "right" and self.captain.y < self.field_size - 1:
-            self.captain.y += 1
-
-        # Collect veggie if present
-        current_pos = self.field[self.captain.x][self.captain.y]
-        if isinstance(current_pos, Veggie):
-            self.captain.addVeggie(current_pos.points)
-            self.veggies.remove(current_pos)
-
-        # Place captain at new position
-        self.field[self.captain.x][self.captain.y] = self.captain
-
-    def moveRabbits(self):
-        for rabbit in self.rabbits:
-            # Remove rabbit from current position
-            self.field[rabbit.x][rabbit.y] = None
-
-            # Move rabbit to new position
-            rabbit.random_move(self.field_size)
-            current_pos = self.field[rabbit.x][rabbit.y]
-
-            # If rabbit lands on veggie, remove veggie
-            if isinstance(current_pos, Veggie):
-                self.veggies.remove(current_pos)
-
-            # Place rabbit at new position
-            self.field[rabbit.x][rabbit.y] = rabbit
-
-    def checkGameOver(self):
+    def check_game_over(self):
+        # Check if the game is over
         return len(self.veggies) == 0
-
-    def start(self):
-        while not self.checkGameOver():
-            self.printField()
-            direction = input("Move Captain (up, down, left, right): ")
-            self.moveCaptain(direction)
-            self.moveRabbits()
-
-            if self.checkGameOver():
-                print("Game Over. Score:", self.captain.veggies_collected)
-                break
