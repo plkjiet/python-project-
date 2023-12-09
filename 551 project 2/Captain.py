0359d113051ead446afd9ac8391c260e323ddff7
plkjiet <70952104+plkjiet@users.noreply.github.com>
2023-12-09 16:46:11 -0500

    Update Captain.py

--- 551 project 2/Captain.py
+++ 551 project 2/Captain.py
@@ -3,8 +3,13 @@ from Creature import Creature
 
 class Captain(Creature):
     def __init__(self, x, y):
-        super().__init__(x, y, "V")
-        self.veggies_collected = 0
+        super().__init__('V', x, y)  # 'V' represents the Captain
+        self.score = 0  # Score of the Captain
 
-    def addVeggie(self, points):
-        self.veggies_collected += points
+    def harvest(self, garden):
+        # Harvest vegetables
+        for veggie in garden.veggies:
+            if veggie.x == self.x and veggie.y == self.y:
+                self.score += veggie.points  # Add points
+                garden.veggies.remove(veggie)  # Remove harvested vegetable
+                break
